c366231c4730dbd15d121f20b5c7b3cc21ac24fc
Parsa <parsamanes@yahoo.com>
2025-09-19 15:18:40 +0300

    Recreate ESS charging landing experience

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import Navbar from '../components/Navbar'
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-slate-950 text-slate-100">
       <Navbar />
-      <main className="flex-1 p-4">
+      <main className="flex-1">
         <Component {...pageProps} />
       </main>
     </div>
